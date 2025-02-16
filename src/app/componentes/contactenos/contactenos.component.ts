481405c22a33761c978839c148d9f0d59179f1ae
Juan Manuel Amador Roa <arroa03@gmail.com>
2025-02-16 15:18:22 -0500

    Add conditional to contact

--- src/app/componentes/contactenos/contactenos.component.ts
+++ src/app/componentes/contactenos/contactenos.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RegistroComponent } from "../registro/registro.component";
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TokenService } from '../../servicios/token.service';
 
 @Component({
   selector: 'app-contactenos',
@@ -11,5 +12,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './contactenos.component.css'
 })
 export class ContactenosComponent {
+  isLoggedIn: boolean = false;
 
+  constructor(
+    private tokenService: TokenService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.tokenService.getIsLoggedIn().subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+    });
+  }
 }
